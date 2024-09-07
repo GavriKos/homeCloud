99646307410fc68035d6fa1e7e46ac40097503a6
GavriKos <gavrikos@yandex.by>
2024-09-07 19:42:37 +0300

    fix after switch

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 from flask import Flask, request, send_file, render_template
-import os
+import os, json
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/share/<md5>')
+def getShare(md5):
     return render_template('index.html')
 
-@app.route('/get_all_images')
-def get_all_images():
-    
-    return "{\"md5_list\":[\"1\",\"2\",\"3\"]}"
+@app.route('/share/all/<md5>')
+def getAllfromShare(md5):
+    data = {}
+    data["mediaList"] = []
+    data["mediaList"].append(4)
+    data["mediaList"].append(5)
+    data["mediaList"].append(6)
+    return json.dumps(data)
 
-@app.route('/share/<md5>')
-def share(md5):
-    num = int(md5)-3
+@app.route('/share/<md5>/<md52>')
+def share(md5,md52):
+    num = int(md52)-3
     image_path = str(num)+'.jpg'
     
     return send_file(image_path, mimetype='image/jpeg')
